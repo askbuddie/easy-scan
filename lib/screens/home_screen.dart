5a5fb9fca2ce6d565994137173c6e99532375736
cimplesid <joshisid178@gmail.com>
2020-07-30 17:00:31 +0545

    Boilerplate code removed

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        child: Text('Home screen'),
+      ),
+    );
+  }
+}

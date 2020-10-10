dca4914d9f7f73d6460421887459407d7f7fa9e9
cimplesid <joshisid178@gmail.com>
2020-10-10 16:43:08 +0545

    History  feature added

--- lib/screens/saved_pdf.dart
+++ lib/screens/saved_pdf.dart
@@ -1,4 +1,7 @@
+import 'dart:io' as dd;
 import 'package:flutter/material.dart';
+import 'package:EasyScan/Utils/constants.dart';
+import 'package:EasyScan/Utils/permission_checker.dart';
 
 class SavedPdfScreen extends StatefulWidget {
   @override
@@ -6,9 +9,72 @@ class SavedPdfScreen extends StatefulWidget {
 }
 
 class _SavedPdfScreenState extends State<SavedPdfScreen> {
+  List<dd.FileSystemEntity> _fileSystemEntitys = [];
+  dd.Directory _savedDir;
+//currrently supports only android
+  Future<void> _getPaths() async {
+    final _per = await permision.checkPermission();
+    if (_per) {
+      _savedDir = dd.Directory(pdfPathAndroid);
+      setState(() {});
+      final _exist = await _savedDir.exists();
+      if (_exist) await _getFileList();
+    }
+  }
+
+  Future<void> _getFileList() async {
+    if (_savedDir != null) {
+      _fileSystemEntitys = _savedDir.listSync();
+      setState(() {});
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _getPaths();
+  }
+
   @override
   Widget build(BuildContext context) {
-    //TODO:make lsit of files using pdfPathAndroid path use openfile package to open
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('History'),
+      ),
+      body: getBody,
+    );
+  }
+
+  Widget get getBody {
+    if (_fileSystemEntitys == null) {
+      return const Center(child: CircularProgressIndicator());
+    } else if (_fileSystemEntitys.isEmpty) {
+      return const Center(
+        child: Text(
+          'No history found',
+          textAlign: TextAlign.center,
+        ),
+      );
+    } else {
+      return GridView.builder(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2, crossAxisSpacing: 4, mainAxisSpacing: 4),
+        itemBuilder: (_, i) {
+          //TODO:make better card
+          return GestureDetector(
+            onTap: () {
+              //TODO:implement open
+            },
+            child: Container(
+                color: primaryColor.withOpacity(0.2),
+                margin: const EdgeInsets.all(10),
+                height: 200,
+                width: 200,
+                child: Center(child: Text("Pdf #${i + 1}"))),
+          );
+        },
+        itemCount: _fileSystemEntitys.length,
+      );
+    }
   }
 }

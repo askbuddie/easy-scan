ba8baf41d548e0d7b1bb65df382babb05847c325
cimplesid <joshisid178@gmail.com>
2020-10-10 16:22:52 +0545

    history button added

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,8 +1,8 @@
+import 'package:EasyScan/screens/saved_pdf.dart';
 import 'package:flutter/material.dart';
 import 'package:EasyScan/widgets/home_card.dart';
 import 'package:EasyScan/screens/images_to_pdf.dart';
 import 'package:EasyScan/screens/scan_and_convert.dart';
-
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -33,6 +33,13 @@ class _HomeScreenState extends State<HomeScreen> {
             iconData: Icons.picture_as_pdf_outlined,
             color: Colors.green,
             text: 'Images to Pdf',
+          ),
+          HomeCard(
+            onTap: () => Navigator.push(
+                context, MaterialPageRoute(builder: (_) => SavedPdfScreen())),
+            iconData: Icons.save_rounded,
+            color: Colors.amber,
+            text: 'History',
           ),
         ],
       ),

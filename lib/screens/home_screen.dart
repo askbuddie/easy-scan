b5e34f37db49d2933c6f9c4f10e87fd4caaf2cff
Astha Nayak <58568514+nayakastha@users.noreply.github.com>
2020-10-12 13:13:24 +0530

    removed commented

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -43,31 +43,6 @@ class _HomeScreenState extends State<HomeScreen> {
               iconData: Icons.save_rounded,
               text: 'History',
             ),
-            // This widget is needed to be implemented for recent projects
-            // Expanded(
-            //     child: Container(
-            //         padding: const EdgeInsets.only(top: 10, left: 10, right: 10),
-            //         margin: const EdgeInsets.only(top: 20),
-            //         decoration: BoxDecoration(
-            //             color: Colors.grey.shade300,
-            //             borderRadius: const BorderRadius.only(
-            //                 topLeft: Radius.circular(50),
-            //                 topRight: Radius.circular(50))),
-            //         child: GridView.builder(
-            //             gridDelegate:
-            //                 const SliverGridDelegateWithFixedCrossAxisCount(
-            //                     crossAxisCount: 2,
-            //                     mainAxisSpacing: 1,
-            //                     crossAxisSpacing: 1),
-            //             itemBuilder: (context, index) {
-            //               return Container(
-            //                   margin: const EdgeInsets.symmetric(horizontal: 10),
-            //                   child: Center(
-            //                     child: Text(
-            //                       index.toString(),
-            //                     ),
-            //                   ));
-            //             })))
           ]),
     );
   }
